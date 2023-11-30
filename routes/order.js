caf0e0fd496fba876e578423a0a6fe72bc7ef01b
curfewUnzipper <cryingpushpa@gmail.com>
2023-12-01 03:14:44 +0530

    Need to perfect some cases

--- routes/order.js
+++ routes/order.js
@@ -2,14 +2,16 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require("mongoose") 
+    //database connection
+mongoose.connect("mongodb://localhost:27017/orderDetails")
+var db= mongoose.connection;
+db.on('error',()=>{console.log("Error in Connecting to Database")})
+db.once('open',()=>{console.log("Connected to Database in ORDERS")})
+
 
 router.get("/",(req,res) =>{
-    res.render('order/order')
-    //database connection
-    mongoose.connect("mongodb://localhost:27017/orderDetails")
-    var db= mongoose.connection;
-    db.on('error',()=>{console.log("Error in Connecting to Database")})
-    db.once('open',()=>{console.log("Connected to Database in ORDERS")})
+res.render('order/order')
+
     })
    
    
@@ -20,27 +22,32 @@ router.get("/test",(req,res) =>{
 
 //order page 
 
-// router.post("/",(req,res)=>{
-//     //console.log(req.body);
-//     var data = [];
-//     for (i=0;i<req.body.name.length;i++){
-//         if (req.body.name[i]!='' && 
-//             req.body.post[i]!='' &&
-//             req.body.size[i]!=''){
-//         data.push({
-//                     "name":req.body.name[i],
-//                     "post":req.body.post[i],
-//                     "size":req.body.size[i]
-//                   })
-//         }else{continue;}
-//     } console.log("Data array ready"+'\n'+JSON.stringify(data));
-//     db.collection("orders").insertMany(data,(err,collection)=>{
-//         if(err){
-//             throw err;
-//         }console.log("Order Placed Successfully")
-//     })
-//     return res.redirect("/shop")
-// })
+router.post("/",(req,res)=>{
+    let keys= Object.keys(req.body);
+    // console.log(keys[0])
+    // console.log(req.body[keys[0]][0])
+    var data = {};
+    var current = {}
+    for (i=0;i<req.body[keys[0]].length;i++){
+        current = {}
+        for(j=0;j<keys.length;j++){
+            current[keys[j]]=req.body[keys[j]][i]
+        }
+        data[`person${i}`]=current
+
+    } console.log("Data array ready"+'\n'+JSON.stringify(data));
+    db.collection("orders").insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }console.log("Order Placed Successfully")
+    })
+
+
+
+
+
+    return res.redirect("/shop")
+})
 
 
 
